10e4efe1eadd69158a2ce057b1baf71dcaf4cb86
ludmiladichiera <ludmiladichiera@gmail.com>
2025-04-20 23:54:11 -0300

    botones atras y siguiente para ver mas imagenes si es que el articulo tiene

--- TPWinForm_equipo-10A/FrmDetalleArticulo.cs
+++ TPWinForm_equipo-10A/FrmDetalleArticulo.cs
@@ -14,7 +14,9 @@ namespace TPWinForm_equipo_10A
 {
     public partial class FrmDetalleArticulo : Form
     {
-        //private List<Articulo> listaArticulo;
+        private Articulo articuloSeleccionado;
+        private int indiceImagen = 0;
+
         public FrmDetalleArticulo()
         {
             InitializeComponent();
@@ -23,34 +25,69 @@ namespace TPWinForm_equipo_10A
         private void FrmDetalleArticulo_Load(object sender, EventArgs e)
         {
             ArticuloNegocio negocio = new ArticuloNegocio();
-            //listaArticulo = negocio.listar();
             dgvArticulos.DataSource = negocio.listar();
-
         }
 
         private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
         {
             if (dgvArticulos.CurrentRow != null)
             {
-                Articulo seleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
+                articuloSeleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
 
-                string imagenUrl = seleccionado.Imagenes != null && seleccionado.Imagenes.Count > 0
-                    ? seleccionado.Imagenes[0].ImagenUrl
-                    : "https://media.istockphoto.com/id/1128826884/es/vector/ning%C3%BAn-s%C3%ADmbolo-de-vector-de-imagen-falta-icono-disponible-no-hay-galer%C3%ADa-para-este-momento.jpg?s=612x612&w=0&k=20&c=9vnjI4XI3XQC0VHfuDePO7vNJE7WDM8uzQmZJ1SnQgk=";
+                ImagenNegocio imagenNegocio = new ImagenNegocio();
+                articuloSeleccionado.Imagenes = imagenNegocio.listar(articuloSeleccionado.Id);
+
+                indiceImagen = 0;
+                mostrarImagen();
+            }
+        }
+
+        private void mostrarImagen()
+        {
+            if (articuloSeleccionado.Imagenes != null && articuloSeleccionado.Imagenes.Count > 0)
+            {
+                string url = articuloSeleccionado.Imagenes[indiceImagen].ImagenUrl;
 
                 try
                 {
-                    pbxArticulo.Load(imagenUrl);
+                    if (!string.IsNullOrWhiteSpace(url) && Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                        pbxArticulo.Load(url);
+                    else
+                        pbxArticulo.Load("https://media.istockphoto.com/id/1128826884/es/vector/ning%C3%BAn-s%C3%ADmbolo-de-vector-de-imagen-falta-icono-disponible-no-hay-galer%C3%ADa-para-este-momento.jpg?s=612x612&w=0&k=20&c=9vnjI4XI3XQC0VHfuDePO7vNJE7WDM8uzQmZJ1SnQgk=");
                 }
                 catch
                 {
-                  
                     pbxArticulo.Load("https://media.istockphoto.com/id/1128826884/es/vector/ning%C3%BAn-s%C3%ADmbolo-de-vector-de-imagen-falta-icono-disponible-no-hay-galer%C3%ADa-para-este-momento.jpg?s=612x612&w=0&k=20&c=9vnjI4XI3XQC0VHfuDePO7vNJE7WDM8uzQmZJ1SnQgk=");
                 }
             }
+            else
+            {
+                pbxArticulo.Load("https://media.istockphoto.com/id/1128826884/es/vector/ning%C3%BAn-s%C3%ADmbolo-de-vector-de-imagen-falta-icono-disponible-no-hay-galer%C3%ADa-para-este-momento.jpg?s=612x612&w=0&k=20&c=9vnjI4XI3XQC0VHfuDePO7vNJE7WDM8uzQmZJ1SnQgk=");
+            }
+        }
 
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            if (articuloSeleccionado != null && articuloSeleccionado.Imagenes != null && articuloSeleccionado.Imagenes.Count > 0)
+            {
+                indiceImagen--;
+                if (indiceImagen < 0)
+                    indiceImagen = articuloSeleccionado.Imagenes.Count - 1;
 
+                mostrarImagen();
+            }
+        }
 
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (articuloSeleccionado != null && articuloSeleccionado.Imagenes != null && articuloSeleccionado.Imagenes.Count > 0)
+            {
+                indiceImagen++;
+                if (indiceImagen >= articuloSeleccionado.Imagenes.Count)
+                    indiceImagen = 0;
+
+                mostrarImagen();
+            }
         }
 
         private void pbxArticulo_Click(object sender, EventArgs e)
@@ -60,17 +97,13 @@ namespace TPWinForm_equipo_10A
 
         private void dgvArticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            /*
+            if (e.RowIndex >= 0)
+            {
+                var articuloSeleccionado = (Articulo)dgvArticulos.Rows[e.RowIndex].DataBoundItem;
 
-        }
-
-        private void btnAtras_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnSiguiente_Click(object sender, EventArgs e)
-        {
-
+                MessageBox.Show($"Artículo seleccionado: {articuloSeleccionado.Nombre}");
+            }*/
         }
     }
 }
